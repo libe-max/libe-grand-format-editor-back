8ccf3411ba343047f372d2aad8b0841e603c48ee
Maxime Fabas <maxime.fabas@pm.me>
2020-06-02 18:26:37 +0200

    create and return real database longforms

--- ws-handlers/handleCreateNewLongform.js
+++ ws-handlers/handleCreateNewLongform.js
@@ -1,7 +1,14 @@
+const initLongformTemplate = {}
+
 function handleCreateNewLongform (socket, io, payload) {
-  console.log('!!!!!!!!!!!!')
-  console.log('CREATE NEW LONGFORM')
-  console.log('!!!!!!!!!!!!')
+  console.log('handleCreateNewLongform', socket.id, payload)
+  const { db } = io
+  const collection = db.collection('longforms')
+  const newLongform = { ...initLongformTemplate }
+  collection.insert(newLongform, (err, res) => {
+    console.log(res)
+  })
+
 }
 
 module.exports = handleCreateNewLongform
--- ws-handlers/handleRequestLongform.js
+++ ws-handlers/handleRequestLongform.js
@@ -1,3 +1,4 @@
+const ObjectId = require('mongodb').ObjectId
 const emitServerError = require('../ws-emiters/emitServerError')
 const emitLongform = require('../ws-emiters/emitLongform')
 
@@ -10,32 +11,9 @@ function handleRequestLongform (socket, io, payload) {
   })
   const { db } = io
   const collection = db.collection('longforms')
-  collection.find({ _id: payload.id }).toArray((e, longforms) => {
+  collection.find({ _id: ObjectId(payload.id) }).toArray((e, longforms) => {
     if (e) emitServerError(socket, io, e.message)
-    else {
-      emitLongform(socket, io, {
-        longform: {
-          _id: payload.id,
-          title: 'Un titre',
-          description: 'Une description',
-          url: 'Une url',
-          author: 'Libé Labo',
-          tweet: 'Some tweet text',
-          show_header: true,
-          image_social: 'https://www.liberation.fr/apps/2020/03/social.jpg',
-          image_call: 'https://www.liberation.fr/apps/2020/03/appel.jpg',
-          image_theme: 'https://www.liberation.fr/apps/2020/03/theme.jpg',
-          blocks: [{
-            type: 'header',
-            bg_image: 'https://www.liberation.fr/apps/2020/04/une-image.jpg',
-            title: 'Un gros titre pour le gros header !',
-            buttons: ['Un', 'Deux', 'Trois']
-          }],
-          meta: {},
-          _changes: {}
-        }
-      })
-    }
+    else emitLongform(socket, io, { longform: longforms[0] })
   })
 }
 
